43ea08a9bf1a4521d7d4f61fcb70a9e7823b3b6e
Yan Bin <ybinbin@outlook.com>
2016-01-05 22:44:38 +0800

    状态回调函数添加
    贝塞尔曲线绘制图形 和原理 值动画的使用和监听

--- app/src/main/java/com/yanbin/sticky/MainActivity.java
+++ app/src/main/java/com/yanbin/sticky/MainActivity.java
@@ -7,23 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.yanbin.sticky.utils.Utils;
+import com.yanbin.sticky.widgest.GooView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        GooView view=new GooView(this);
+//        setContentView(R.layout.activity_main);
+        setContentView(view);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+       view.setOnDragChangedListener(new GooView.OnDragChangedListener() {
+           @Override
+           public void onDisappear() {
+               Utils.showToast(MainActivity.this,"消失了");
+           }
+
+           @Override
+           public void onReset(boolean isOutofRange) {
+               Utils.showToast(MainActivity.this,"恢复了"+isOutofRange);
+           }
+       });
     }
 
 }
--- app/src/main/java/com/yanbin/sticky/widgest/GooView.java
+++ app/src/main/java/com/yanbin/sticky/widgest/GooView.java
@@ -1,14 +1,21 @@
 package com.yanbin.sticky.widgest;
 
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.yanbin.sticky.utils.GeometryUtil;
 import com.yanbin.sticky.utils.Utils;
@@ -41,13 +48,17 @@ public class GooView extends View{
     };
 
     PointF mControlPoint = new PointF(150f, 300f);
+    //两圆最远距离
+    private float farestDistance =80f;
+    private boolean isOutOfRange;
+    private boolean isDisappear;
 
     public GooView(Context context) {
         this(context,null);
     }
 
     public GooView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public GooView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -57,7 +68,20 @@ public class GooView extends View{
         mPaint.setColor(Color.RED);
        // int actionBarHeight =Context.getActionBar().getHeight();
     }
+    //定义回调
+    public interface OnDragChangedListener{
+        void onDisappear();
+        void onReset(boolean isOutofRange);
+    }
+    private OnDragChangedListener onDragChangedListener;
 
+    public OnDragChangedListener getOnDragChangedListener() {
+        return onDragChangedListener;
+    }
+
+    public void setOnDragChangedListener(OnDragChangedListener onDragChangedListener) {
+        this.onDragChangedListener = onDragChangedListener;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -65,6 +89,9 @@ public class GooView extends View{
             // 计算变量的值
 
             // 0. 求出实时的固定圆半径
+                float tempStickyCenterRadius=getStickRadius();
+                mStickRadius=tempStickyCenterRadius;
+
         // 1. 四个点附着点的坐标
         float yOffset=mDragCenter.y-mStickCenter.y;
         float xOffset=mDragCenter.x-mStickCenter.x;
@@ -81,55 +108,84 @@ public class GooView extends View{
 
         // 保存当前状态, 向上平移状态栏的高度, 以使其与手指按下点重合
         canvas.save();
-        canvas.translate(0,-statusBarHeight);
+        canvas.translate(0, -statusBarHeight);
 
-        //画中间连接部分
-        Path path=new Path();
-        //贝塞尔曲线 指定开始节点，结束节点，和控制点
-        //结合图片看
-        //跳到点1  替换为定义的变量
-        path.moveTo(mStickPoints[0].x, mStickPoints[0].y);
-        //1-->2
-        //指定控制点 和 结束点 画曲线
-        path.quadTo(mControlPoint.x,mControlPoint.y,mDragPoints[0].x,mDragPoints[0].y);
-        //2->3
-        path.lineTo(mDragPoints[1].x, mDragPoints[1].y);
-        //3--》4
-        path.quadTo(mControlPoint.x,mControlPoint.y,mStickPoints[1].x,mStickPoints[1].y);
-        //二阶贝塞尔曲线
+            //画最大范围的参考圆环
+        mPaint.setStyle(Paint.Style.STROKE);//只画圆环
+        canvas.drawCircle(mStickCenter.x, mStickCenter.y, farestDistance, mPaint);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        if(!isDisappear) {//只有没有消失才绘制
+            //如果没有超出范围 画固定圆和连接部分
+            if (!isOutOfRange) {
+                //画中间连接部分
+                Path path = new Path();
+                //贝塞尔曲线 指定开始节点，结束节点，和控制点
+                //结合图片看
+                //跳到点1  替换为定义的变量
+                path.moveTo(mStickPoints[0].x, mStickPoints[0].y);
+                //1-->2
+                //指定控制点 和 结束点 画曲线
+                path.quadTo(mControlPoint.x, mControlPoint.y, mDragPoints[0].x, mDragPoints[0].y);
+                //2->3
+                path.lineTo(mDragPoints[1].x, mDragPoints[1].y);
+                //3--》4
+                path.quadTo(mControlPoint.x, mControlPoint.y, mStickPoints[1].x, mStickPoints[1].y);
+                //二阶贝塞尔曲线
 //        path.cubicTo();
-        //会自动封闭
-        path.close();//将图形封闭
+                //会自动封闭
+                path.close();//将图形封闭
 
-        canvas.drawPath(path, mPaint);
+                canvas.drawPath(path, mPaint);
+                //画固定圆
+                canvas.drawCircle(mStickCenter.x, mStickCenter.y, mStickRadius, mPaint);
 
-        // 画附着点(参考用)
-        mPaint.setColor(Color.BLUE);
-        canvas.drawCircle(mDragPoints[0].x, mDragPoints[0].y, 3f, mPaint);
-        canvas.drawCircle(mDragPoints[1].x, mDragPoints[1].y, 3f, mPaint);
-        canvas.drawCircle(mStickPoints[0].x, mStickPoints[0].y, 3f, mPaint);
-        canvas.drawCircle(mStickPoints[1].x, mStickPoints[1].y, 3f, mPaint);
-        mPaint.setColor(Color.RED);
+                // 画附着点(参考用)
+                mPaint.setColor(Color.BLUE);
+                canvas.drawCircle(mDragPoints[0].x, mDragPoints[0].y, 3f, mPaint);
+                canvas.drawCircle(mDragPoints[1].x, mDragPoints[1].y, 3f, mPaint);
+                canvas.drawCircle(mStickPoints[0].x, mStickPoints[0].y, 3f, mPaint);
+                canvas.drawCircle(mStickPoints[1].x, mStickPoints[1].y, 3f, mPaint);
+                mPaint.setColor(Color.RED);
+            }
 
+            //画拖拽圆
+            canvas.drawCircle(mDragCenter.x, mDragCenter.y, mDragRadius, mPaint);
 
-
-        //画拖拽圆
-        canvas.drawCircle(mDragCenter.x,mDragCenter.y,mDragRadius,mPaint);
-
-        //画固定圆
-        canvas.drawCircle(mStickCenter.x,mStickCenter.y,mStickRadius,mPaint);
+        }
 
 
         canvas.restore();//恢复为之前保存的状态
     }
+
+    /**
+     * 获取固定圆变化的半径
+     * @return
+     */
+    private float getStickRadius() {
+        //开始距离
+        float distanceBetween2Points = GeometryUtil.getDistanceBetween2Points(mDragCenter, mStickCenter);
+        //0--》farest 80f
+        distanceBetween2Points=Math.min(distanceBetween2Points, farestDistance);
+        //0.0f -> 1.0f (12f->4f ) 固定圆半径由最大逐渐
+        float percent=distanceBetween2Points/ farestDistance;
+        Float stickyRadius = evaluate(percent, 12f, 4f);
+        return  stickyRadius;
+    }
+    public Float evaluate(float fraction, Number startValue, Number endValue) {
+        float startFloat = startValue.floatValue();
+        return startFloat + fraction * (endValue.floatValue() - startFloat);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float rawX;
         float rawY;
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-//                isDisappear = false;
-//                isOutOfRange = false;
+                isDisappear = false;
+                isOutOfRange = false;
 
                 rawX = event.getRawX();
                 rawY = event.getRawY();
@@ -141,70 +197,77 @@ public class GooView extends View{
                 rawY = event.getRawY();
                 updateDragCenter(rawX, rawY);
 //
-//                // 拖拽过程中 判断两圆心距离
-//                float distance = GeometryUtil.getDistanceBetween2Points(mDragCenter, mStickCenter);
-//                if(distance > farestDistance){
-//                    isOutOfRange  = true;
+                // 拖拽过程中 判断两圆心距离
+                float distance = GeometryUtil.getDistanceBetween2Points(mDragCenter, mStickCenter);
+                if(distance > farestDistance){
+                    isOutOfRange  = true;
                     invalidate();
-//                }
+
+//                    if(onDragChangedListener!=null)
+//                        onDragChangedListener.onDisappear();
+
+                }
 
                 break;
             case MotionEvent.ACTION_UP:
 
-//                if(isOutOfRange){
-//                    // 刚刚超出范围了
-//                    float d = GeometryUtil.getDistanceBetween2Points(mDragCenter, mStickCenter);
-//                    if(d > farestDistance){
-//                        // 1.松手时还在圈外 消失
-//                        isDisappear  = true;
-//                        invalidate();
-//
-//                        if(onDragChangeListener != null){
-//                            onDragChangeListener.onDisappear();
-//                        }
-//
-//                    }else {
+                if(isOutOfRange){
+                    // 刚刚超出范围了 松手再去获取两圆距离
+                    float d = GeometryUtil.getDistanceBetween2Points(mDragCenter, mStickCenter);
+                    if(d > farestDistance){
+                        // 1.松手时还在圈外 松手时消失
+                        isDisappear  = true;
+                        invalidate();
+
+                        if(onDragChangedListener!=null)
+                            onDragChangedListener.onDisappear();
+
+                    }else {
 //                        // 2.松手时放回圈里
-//                        updateDragCenter(mStickCenter.x, mStickCenter.y);
+                        updateDragCenter(mStickCenter.x, mStickCenter.y);
 //
-//                        if(onDragChangeListener != null){
-//                            onDragChangeListener.onReset(true);
-//                        }
-//                    }
-//                }else {
-//                    // 松手时没超出范围, 弹回去
+                        if(onDragChangedListener!=null)
+                            onDragChangedListener.onReset(true);
+                    }
+                }else {
+                    // 松手时没超出范围, 弹回去 使用动画
+                    //弹回去意思是 拖拽圆逐渐回弹到固定圆 根据动画执行时间不断改变拖拽圆圆心 绘制界面
+                    //开始point
+                    final PointF startP = new PointF(mDragCenter.x, mDragCenter.y);
 //
-//                    final PointF startP = new PointF(mDragCenter.x, mDragCenter.y);
-//
-//                    ValueAnimator anim = ValueAnimator.ofFloat(10f);
-//                    anim.addUpdateListener(new AnimatorUpdateListener() {
-//
-//                        @Override
-//                        public void onAnimationUpdate(ValueAnimator mAnim) {
-//                            Float animatedValue = (Float) mAnim.getAnimatedValue();
-//                            float percent = mAnim.getAnimatedFraction();
-//                            // 0.0f -> 1.0f
-//                            System.out.println("percent: " + percent + " animatedValue: " + animatedValue);
-//                            // 根据百分比获取从 拖拽圆圆心到固定圆圆心的中间某个点坐标
-//                            PointF p = GeometryUtil.getPointByPercent(startP, mStickCenter, percent);
-//
-//                            updateDragCenter(p.x, p.y);
-//                        }
-//                    });
-//                    anim.addListener(new AnimatorListenerAdapter() {
-//                        @Override
-//                        public void onAnimationEnd(Animator animation) {
-//
-//                            if(onDragChangeListener != null){
-//                                onDragChangeListener.onReset(false);
-//                            }
-//                        }
-//                    });
-//                    //Simple , Base, Default, Adapter, Basic
-//                    anim.setInterpolator(new OvershootInterpolator(2));
-//                    anim.setDuration(300);
-//                    anim.start();
-//                }
+                    ValueAnimator anim = ValueAnimator.ofFloat(10f);
+                    anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+                        @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator mAnim) {
+                            Float animatedValue = (Float) mAnim.getAnimatedValue();
+                            //动画从percent
+                            float percent = mAnim.getAnimatedFraction();
+                            // 0.0f -> 1.0f
+                            System.out.println("percent: " + percent + " animatedValue: " + animatedValue);
+                            // 根据百分比获取从 拖拽圆圆心到固定圆圆心的中间某个点坐标
+                            //开始点x不断接近结束点x 开始点y不断接近结束点y
+                            PointF p = GeometryUtil.getPointByPercent(startP, mStickCenter, percent);
+
+                            updateDragCenter(p.x, p.y);
+                        }
+                    });
+                    //Simple , Base, Default, Adapter, Basic
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            //动画执行完毕执行回调
+                            if(onDragChangedListener!=null)
+                                onDragChangedListener.onReset(false);
+                        }
+                    });
+
+                    //OvershootInterpolator 类似于皮筋的张力
+                    anim.setInterpolator(new OvershootInterpolator(2));
+                    anim.setDuration(300);
+                    anim.start();
+                }
 
                 break;
 
